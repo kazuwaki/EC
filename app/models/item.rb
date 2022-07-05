0f5e94abb6116d290259ec2e7305ffe6b428ddcf
EC2 Default User <104773179+kazuwaki@users.noreply.github.com>
2022-07-05 13:27:20 +0000

    [finish]admin/items

---
+++ app/models/item.rb
@@ -0,0 +1,16 @@
+class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :genre
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def text_price
+    (price*1.1)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    resources :items, only: [:new, :show, :index, :create, :update, :edit ]
+
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
