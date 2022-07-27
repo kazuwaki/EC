e423680333202f42f40b221304713cfa21c4ae33
EC2 Default User <104773179+kazuwaki@users.noreply.github.com>
2022-07-27 14:56:01 +0000

    [Add]Rails日本語化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery";
+import "./sample"
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
-import "./slick"
+
+
 
 Rails.start()
 Turbolinks.start()
