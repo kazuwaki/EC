cf4ca44e31f4d808d6853cec5c233dfac2f26f44
EC2 Default User <104773179+kazuwaki@users.noreply.github.com>
2022-07-28 07:11:59 +0000

    [Add]会員側のバリデーション

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,6 +7,17 @@ class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :addresses, dependent: :destroy
 
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :address, presence: true
+  validates :postal_code, presence: true
+  validates :telephone_number, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :password, presence: true
+
   def active_for_authentication?
     super && (is_deleted == false)
   end
