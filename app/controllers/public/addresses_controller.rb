4114124d30a5be27746a8cda02196da90caf4083
EC2 Default User <104773179+kazuwaki@users.noreply.github.com>
2022-07-28 10:58:32 +0000

    [Add]配送先登録のバリデーション、非同期化

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -5,11 +5,13 @@ class Public::AddressesController < ApplicationController
   end
 
   def create
-    @addresses = current_customer.addresses.where.not(post_code: nil, name: nil, address: nil)
+    @addresses = current_customer.addresses.where.not(postal_code: nil, name: nil, address: nil)
     @address = Address.new
     @address = current_customer.addresses.new(address_params)
-    @address.save
-    redirect_to request.referer
+    unless @address.save
+      render "error"
+    end
+    @address_new = Address.new
   end
 
   def edit
@@ -23,9 +25,9 @@ class Public::AddressesController < ApplicationController
   end
 
   def destroy
+    @addresses = current_customer.addresses.where.not(postal_code: nil, name: nil, address: nil)
     @address = Address.find(params[:id])
     @address.destroy
-    redirect_to request.referer
   end
 
   def address_params
